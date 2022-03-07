64aada1376f6e9d23ffa2da817059ed96d0f835d
Bora Kaya <bora.587@hotmail.com>
2022-03-07 20:55:11 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    internal class Program
+    {
+        public static int ArttırVeTopla(ref int x, ref int y)
+        {
+            x += 1;
+            y += y;
+            return (x + y);
+        }
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+            int sonuc =ArttırVeTopla(ref x,ref y);
+            Console.WriteLine(sonuc);
+            Console.ReadKey();  
+
+
+        }
+    }
+}
